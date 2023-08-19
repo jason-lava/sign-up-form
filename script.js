5a992e4cb546434b42d153dbce96ebe88be64b4d
jason-lava-rev <lavandowskij@outlook.com>
2023-08-19 14:21:46 -0700

    add verification that passwords match

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
-// assign js consts to the password and confirm password html fields
-const password1 = document.getElementById('#password');
-const password2 = document.getElementById('#confirmPassword');
+// ensure passwords match
+function passwordMatch() {
 
-// criteria: min 8 chars, 1 of each: uppercase, lowercase, symbol, number
-const regexPassword = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/;
+    const password1 = document.getElementById('password');
+    const password2 = document.getElementById('confirmPassword');
+    
+    if(password1.value != password2.value) {
+        password2.setCustomValidity('Password Must be Matching.');
+    } else {
+        password2.setCustomValidity('');
+    }
 
-// validate that passwords match via regex
-function regexValidate(password1, password2) {
-    console.log(password1, password2)
-    if (!regexPassword.test(password1)) {
-        return false;
-    }
-    if (!regexPassword.test(password2)) {
-        return false;
-    }
-    if (password1 !== password2) {
-        return false;
-    } else if (password1 === password2) {
-        return true;
-    }
 }
-
-regexValidate(password1, password2)
